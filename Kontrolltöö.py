0ff3212d8a80e988daed559dbe0cb4dfc4d51369
opilane <opilane@HP-CZC2349HTB>
2025-10-30 10:02:20 +0200

    kontroll töö 1

---
+++ Kontrolltöö.py
@@ -0,0 +1,38 @@
+
+#esimene ülesanne
+#n = int(input("Введите количество ёлок (1–9): "))
+#
+#kuusk = [
+#    "   /V\\   ",
+#    "  / V \\  ",
+#    " / V V \\ ",
+#    "/VV V VV\\"
+#]
+#
+#sep = " "
+#
+#for rida in kuusk:
+#    print(sep.join([rida] * n))
+
+
+
+#teine ülesanne
+#R = int(input("Введите число R: "))
+#
+#korrutis = 1
+#
+#for i in range(1, R+1):
+#    if i % 2!= 0:
+#        korrutis *= i
+#
+#print(f"Произведение всех нечетных чисел равно: {korrutis}")
+
+#kolme ülesanne
+num = int(input("Введите числа через пробел:"))
+positivsed = 0
+for arv in num:
+    if int(arv) > 0:
+        positivsed += 1
+
+print(f"Позтитвынх чисел {positivsed}")
+
