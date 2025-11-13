86356efd874f1ed572b45014cca7da0a47660d09
opilane <opilane@HP-CZC2349HV2>
2025-11-13 09:35:47 +0200

    try + tsüklid

--- Kontrolltöö.py
+++ Kontrolltöö.py
@@ -1,66 +1,88 @@
 
 #esimene ülesanne
-#n = int(input("Введите количество ёлок (1–9): "))
-#if n > 0 and n < 10:    
-#     kuusk = [
-#     "   /V\\   ",
-#     "  / V \\  ",
-#     " / V V \\ ",
-#     "/VV V VV\\"
-#     ]
-#
-#     for rida in kuusk:
-#         print((" " + rida) * n)
-#else:
-#    print("Нельзя вводить число меньше 1 или больше 9")
+try:
+    n = int(input("Sisestage puude arv (1–9): "))
+    
+    if 1 <= n <= 9:
+        kuusk = [
+            "   /V\\   ",
+            "  / V \\  ",
+            " / V V \\ ",
+            "/VV V VV\\"
+        ]
+        
+        for rida in kuusk:
+            print((rida + " ") * n)
+    else:
+        print("Sa ei saa sisestada numbrit, mis on väiksem kui 1 ega suurem kui 9.")
+except ValueError:
+    print("Vaja on täisarvu")
 
 
 
 
 #teine ülesanne
-#R = int(input("Введите число R: "))
-#rez = 1 #обнулит
-#
-#for i in range(1, R+1): #от 1 до R
-#    if i % 2 != 0: #не четное
-#        rez = rez * i #умножаем
-#
-#print(f"Произведение всех нечетных чисел равно: {rez}")
+try:
+    R = int(input("Sisesta number R:"))
+    rez = 1 
+
+    for i in range(1, R+1): 
+        if i % 2 != 0:
+            rez = rez * i 
+
+    print(f"Kõigi paaritute arvude korrutis on: {rez}")
+except Exception:
+    print("viga")
 
 
 #kolme ülesanne
-#num = input("Введите числа через пробел:").split() #делит строку
-#positivsed = 0 #счетчик положительных
-#for arv in num:
-#    if int(arv) > 0: #текст в число
-#        positivsed += 1 #+1 если положительное
-#    else:
-#        print("Числа должны быть положительные!")
-#
-#print(f"Позтитвынх чисел {positivsed}")
+try:
+    num = input("Sisseta numbrid: ")
+    positivsed = 0 
+    for arv in num:
+        try:
+            value = int(arv)
+            if value > 0: 
+                positivsed += 1 
+            else:
+                print("Numbrid peavad olema positivsed!")
+        except ValueError:
+            print(f"Viga! {value} ei ole num!")
+
+    print(f"Позтитвынх чисел {positivsed}")
+except Exception:
+    print("Viga!")
+
+
 
 #nelja ülesanne 
-#arv = input("Введите число: ")
-#
-#chet = 0
-#ne_chet = 0
-#
-#for num in arv:
-#    num =int(num)
-#    if num % 2 == 0: #четное или не четное
-#        chet += 1
-#    else:
-#        ne_chet += 1
-#
-#print(f"Четных чисел: {chet}, нечетных {ne_chet}")
+try:
+    arv = input("Sisseta numbrid: ")
+
+    chet = 0
+    ne_chet = 0
+
+    for num in arv:
+        num =int(num)
+        if num % 2 == 0: 
+            chet += 1
+        else:
+            ne_chet += 1
+
+    print(f"paarisarvud: {chet}, veider {ne_chet}")
+except Exception:
+    print("Value!")
 
 
 #viies ülesanne
-#A = int(input("Введите первое число: "))
-#B = int(input("Введите второе число: "))
-#
-#summa = 0
-#
-#for i in range(A, B + 1): #создает все числа от A до B
-#    summa += i #добовляем каждое число
-#print(f"Сумма чисел от {A} до {B } равна {summa}")
+try:
+    A = int(input("Введите первое число: "))
+    B = int(input("Введите второе число: "))
+
+    summa = 0
+
+    for i in range(A, B + 1): 
+        summa += i 
+    print(f"Сумма чисел от {A} до {B } равна {summa}")
+except Exception:
+    print("Value!")
