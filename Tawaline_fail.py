dda9de55a5233c024e0c183785337de98b065f53
opilane <opilane@HP-CZC2349HT0>
2025-12-04 10:02:53 +0200

    tawaline upgrade

--- Tawaline_fail.py
+++ Tawaline_fail.py
@@ -2,18 +2,9 @@
 
 print("Lanedamine mitu arvutehet!")
 for i in range(5):
-    while True:
-        try:
-            arv1 = float(input("Sisseta esimene arv: "))
-            break
-        except ValueError:
-            print("Plaun sisseta arv!")
-    while True:
-        try:
-            arv2 = float(input("Sisseta teine arv: "))
-            break
-        except ValueError:
-            print("Plaun sisseta arv!")
-t=input("Sisseta tehe (+ , - , * , /)")
-tulemus=arithmetic(arv1, arv2, t)
-print(f"{arv1}{t}{arv2}={tulemus}")
+    arv1 = float_kontroll(input("Sisesta esimene arv: "))
+    arv2 = float_kontroll(input("Sisesta teine arv: "))
+
+    t=input("Sisseta tehe (+ , - , * , /)")
+    tulemus=arithmetic(arv1, arv2, t)
+    print(f"{arv1} {t} {arv2} = {tulemus}")
