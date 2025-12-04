fcf8916d3bfb32b0298072bd96b1c35725bc85ef
opilane <opilane@HP-CZC2349HT0>
2025-12-04 09:59:44 +0200

    mooduli

---
+++ Mooduli_fail.py
@@ -0,0 +1,39 @@
+﻿#5.1
+#esimene ülesanne
+
+def float_kontroll(sisend:str)->float:
+     while True:
+        try:
+            arv = float(sisend)
+            return arv
+        except ValueError:
+            sisend=input("Plaun sisseta arv!")
+
+
+
+
+
+
+
+
+def arithmetic(a:float,b:float,tehe:str)->any:
+    """Lihtne kalkulator:
+    + - liitmine
+    - - lahuramine
+    * - korrutamine
+    / - jagamine
+    Muul juhul tagastab "Tundmatu tehe"
+    :param float a: esimene arv
+    :param float: b teine arv
+    :param srt tehe: tehe, mis tuleb teha
+    :return/rtype: tehte tulemus float või str
+    """
+
+    if tehe in ["+", "-", "*", "/"]:
+        if tehe=="/" and b == 0:
+            vastus = "Nulliga jagamine pole lubatud"
+        else:
+            vastus = eval(f"{a}{tehe}{b}")
+    else:
+        vastus="Tundmatu tehe"
+    return vastus
---
+++ Tawaline_fail.py
@@ -0,0 +1,19 @@
+﻿from Mooduli_fail import *
+
+print("Lanedamine mitu arvutehet!")
+for i in range(5):
+    while True:
+        try:
+            arv1 = float(input("Sisseta esimene arv: "))
+            break
+        except ValueError:
+            print("Plaun sisseta arv!")
+    while True:
+        try:
+            arv2 = float(input("Sisseta teine arv: "))
+            break
+        except ValueError:
+            print("Plaun sisseta arv!")
+t=input("Sisseta tehe (+ , - , * , /)")
+tulemus=arithmetic(arv1, arv2, t)
+print(f"{arv1}{t}{arv2}={tulemus}")
