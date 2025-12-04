126067f96662f1a3f01d4c97ad9b0a2ecc02110a
opilane <opilane@HP-CZC2349HT0>
2025-12-04 11:12:13 +0200

    square

--- Mooduli_fail.py
+++ Mooduli_fail.py
@@ -1,5 +1,9 @@
 ﻿#5.1
 #esimene ülesanne
+from importlib.metadata import diagnose
+import math
+
+
 
 def float_kontroll(sisend:str)->float:
      while True:
@@ -10,9 +14,13 @@ def float_kontroll(sisend:str)->float:
             sisend=input("Plaun sisseta arv!")
 
 
-
-
-
+def int_kontroll(sisend:str)->int:
+     while True:
+        try:
+            arv = int(sisend)
+            return arv
+        except ValueError:
+            sisend=input("Plaun sisseta arv!")
 
 
 
@@ -37,3 +45,18 @@ def arithmetic(a:float,b:float,tehe:str)->any:
     else:
         vastus="Tundmatu tehe"
     return vastus
+
+
+
+def is_year_leap(year: int) -> bool:
+   if (year % 4 == 0 and year % 100 != 0):
+       return True
+   else:
+        return False
+
+
+def square(külg: float) -> any:
+    perimitor = 4 *külg
+    pindala = külg * külg
+    diagonal = külg * math.sqrt(2)
+    return perimitor, pindala, diagonal 
--- Tawaline_fail.py
+++ Tawaline_fail.py
@@ -1,10 +1,32 @@
-﻿from Mooduli_fail import *
+﻿from msvcrt import kbhit
+from tkinter.filedialog import dialogstates
+from Mooduli_fail import *
 
-print("Lanedamine mitu arvutehet!")
-for i in range(5):
-    arv1 = float_kontroll(input("Sisesta esimene arv: "))
-    arv2 = float_kontroll(input("Sisesta teine arv: "))
+#print("Lanedamine mitu arvutehet!")
+#for i in range(5):
+#    arv1 = float_kontroll(input("Sisesta esimene arv: "))
+#    arv2 = float_kontroll(input("Sisesta teine arv: "))
+#
+#    t=input("Sisseta tehe (+ , - , * , /)")
+#    tulemus=arithmetic(arv1, arv2, t)
+#    print(f"{arv1} {t} {arv2} = {tulemus}")
 
-    t=input("Sisseta tehe (+ , - , * , /)")
-    tulemus=arithmetic(arv1, arv2, t)
-    print(f"{arv1} {t} {arv2} = {tulemus}")
+
+
+#for i in range(5):
+#    year = int_kontroll(input("Sisseta aasta: "))
+#    if is_year_leap(year):
+#        print(f"{year} on ligaasta!")
+#    else:
+#        print(f"{year} on tavaline aasat!")
+
+
+#for i in range(5):
+#    külg = float_kontroll(input("Siseta ruudu külje pikkuse: "))
+#    perimitor,pindala,diagonal=square(külg)
+#    print(f"Ruudu külg: {külg}")
+#    print(f"Ruudu perimitor: {perimitor}")
+#    print(f"Ruudu ümbermõtt: {pindala}")
+#    print(f"Ruudu dioganal: {diagonal}")
+
+
