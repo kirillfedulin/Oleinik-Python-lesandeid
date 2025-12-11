e61a94e647086c2968bd78fd716f8ca26b533a35
opilane <opilane@HP-CZC2349HTY>
2025-12-11 11:35:09 +0200

    5.2

---
+++ 5.2/MyModle.py
@@ -0,0 +1,71 @@
+﻿k = []
+s = []
+
+def regestreerimine(k: list, s: list)->any:
+    while True:
+        nimi = input("\nSisesta uus kasutajanimi: ")
+        if nimi in k:
+            print("Nimi juba võetud!")
+            continue
+
+        parool = input("Sisesta parool: ")
+        if len(parool) < 4:
+            print("\nNõrk parool!")
+            continue
+
+        k.append(nimi)
+        s.append(parool)
+        print("\nRegistreerimine õnnestus!\n")
+        return
+
+
+def autorimine(k: list, s: list)->any:
+    while True:
+        nimi = input("\nSisesta kasutajanimi: ")
+        parool = input("Sisesta parool: ")
+
+        if nimi not in k:
+            print("\nSellist kasutajat pole!")
+            continue
+
+        i = s.index(nimi)
+        if s[i] != parool:
+            print("\nVale parool!")
+            continue
+
+        print("\nSisselogimine õnnestus!\n")
+        return nimi
+
+
+def parolivahetus(k: list, s: list, ks: list)->any:
+    i = k.index(ks)
+    print("\n1 - Muuda nime")
+    print("2 - Muuda parooli\n")
+    valik = input("Valik: ")
+
+    if valik == "1":
+        uus_nimi = input("\nUus nimi: ")
+        if uus_nimi in k:
+            print("\nNimi juba võetud!\n")
+        else:
+            k[i] = uus_nimi
+            print("\nNimi muudetud!\n")
+            return uus_nimi
+    elif valik == "2":
+        uus_parool = input("\nUus parool: ")
+        s[i] = uus_parool
+        print("\nParool muudetud!\n")
+
+    return ks
+
+
+def paroolitaastamine(k: list, s: list)->any:
+    nimi = input("\nSisesta kasutajanimi: ")
+    if nimi not in k:
+        print("\nSellist kasutajat pole!\n")
+        return
+
+    i = k.index(nimi)
+    uus = input("Sisesta uus parool: ")
+    s[i] = uus
+    print("Parool taastatud!")
---
+++ 5.2/MyTawaline.py
@@ -0,0 +1,28 @@
+﻿from MyModle import *
+
+
+k = []
+s = []
+
+
+while True:
+    valik = 0
+    print("Menu")
+    print("Sisseta palun mis te tahate: ")
+    print("1 - regestreerimine")
+    print("2 - Autoriserumine")
+    print("3 - Nime muutmine")
+    print("4 - Parooli taastamine")
+    print("5 - Lõpetamine")
+    ask = input(" ")
+    
+    if ask == "1":
+        regestreerimine(k, s)
+    elif ask == "2":
+        autorimine(k, s)
+    elif ask == "3":
+        parolivahetus(k, s)
+    elif ask == "4":
+        paroolitaastamine(k, s)
+    elif ask == "5":
+        break
