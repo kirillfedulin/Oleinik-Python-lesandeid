0a27534e06f6be7348492a759579e0057cb20776
Kirill Fedulin <crazzyhd22@gmail.com>
2025-12-18 12:03:22 +0200

    Refactor menu and function calls in MyTawaline.py

--- 5.2/MyTawaline.py
+++ 5.2/MyTawaline.py
@@ -1,32 +1,40 @@
-﻿from MyModle import *
+from moodul import *
+
+def main():
+    s = []   
+    k = [] 
+
+    while True:
+        print("\n1 - Registreerimine")
+        print("2 - Autoriseerimine")
+        print("3 - Nime/parooli muutmine")
+        print("4 - Unustatud parool")
+        print("5 - Lõpetamine")
+
+        valik = input("Valik: ")
+
+        if valik == "1":
+            registreeri(s, k)
+
+        elif valik == "2":
+            current_user = autoriseeri(s, k)
+
+        elif valik == "3":
+            if current_user is None:
+                print("Pole sisse logitud!")
+            else:
+                current_user = muuda(s, k, current_user)
+
+        elif valik == "4":
+            unustatud_parool(s, k)
+
+        elif valik == "5":
+            print("Programmi lõpp.")
+            break
+
+        else:
+            print("Vale valik!")
 
 
-k = []
-s = []
-
-
-while True:
-    valik = 0
-    print("Menu")
-    print("Sisseta palun mis te tahate: ")
-    print("1 - regestreerimine")
-    print("2 - Autoriserumine")
-    print("3 - Nime muutmine")
-    print("4 - Parooli taastamine")
-    print("5 - Lõpetamine")
-    ask = input(" ")
-    
-    if ask == "1":
-        ask = input("Kas te tahate regestreerimine sinu paaroliga või random?")
-        if ask == "random":
-            salasona(k) 
-        elif ask == "minu":
-            regestreerimine(k, s)
-    elif ask == "2":
-        autorimine(k, s)
-    elif ask == "3":
-        parolivahetus(k, s)
-    elif ask == "4":
-        paroolitaastamine(k, s)
-    elif ask == "5":
-        break
+if __name__ == "__main__":
+    main()
