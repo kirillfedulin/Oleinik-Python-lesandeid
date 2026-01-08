ef5e39b18587ee856edd28c7fa3af5ce7bf551bd
Kirill Fedulin <crazzyhd22@gmail.com>
2026-01-08 13:53:15 +0200

    Update Töö Tekstifailidega ja Kaustadega.py

--- Töö Tekstifailidega ja Kaustadega.py
+++ Töö Tekstifailidega ja Kaustadega.py
@@ -1,17 +1,22 @@
-﻿def kirjuta_falisse(failinimi:str, loend:list):
+def kirjuta_falisse(failinimi:str, loend:list):
     reziim = input("Sisseta faili avamise reziim (w - kirjutamine, a - lisamine): ")
-    n=len(loend)
-    if n!=0:
-        for e in loend:
-            print(e)
-    for i in range(10):
+    for i in range(5):
         element=input("Sisseta tekst: ")
-        loend.append(f"\n {element}")
+        loend.append(element)
     with open(failinimi+"txt",reziim,encoding="uft=8") as f:
-        f.writelines(loend)
+        for rida in loend:
+            f.write(rida + "\n")
+
+def loe_failist(failinimi: str)->list:
+    leond = []
+    with open(failinimi+".txt", "r", encoding="utf=8") as f:
+        for rida in f:
+            loend.append(rida.strip())
+        return loend
 
 
 
 loend=["Rida 1", "Rida 2"]
 failinimi=input("Sisesta faili nimi: ")
 kirjuta_falisse(failinimi, loend)
+
