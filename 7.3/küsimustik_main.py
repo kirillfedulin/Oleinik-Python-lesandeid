97e3a147d3ee52e56b2b2e646bef1de6a54ea932
Kirill Fedulin <crazzyhd22@gmail.com>
2026-01-16 14:07:34 +0200

    Implement data reading from file function
    Added function to read data from a file and store questions and answers.

--- 7.3/küsimustik_main.py
+++ 7.3/küsimustik_main.py
@@ -1,7 +1,23 @@
-from moodule import *
+def andmete_lugemine_failidest():
+    with open("faili_küsimused.txt ", "r") as f:
+        read =[]
+        for rida in f:
+            read.append(rida)
 
-andmete_lugemine_failidest()
-nimi=("Sissesta oma nimi: ")
-print(f"{nimi} ! Tere tulemast küsimustikuu! ")
+        kus_vas ={}
+        for rida in read:
+            kysimus, vastus = rida.split()
+
+def admete_salvesatmine_failidesse():
+    pass
+
+def tesimine():
+    pass
+
+def email_saatmine():
+    pass
+
+def küsimuste_lisamine():
+    pass
 
 
