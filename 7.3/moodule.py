a7592306ded7f1fd9de4a52bfc685624e70cd89a
Kirill Fedulin <crazzyhd22@gmail.com>
2026-01-16 13:14:38 +0200

    Update moodule.py

--- 7.3/moodule.py
+++ 7.3/moodule.py
@@ -1,1 +1,14 @@
+def andmete_lugemine_failidest():
+    pass
 
+def admete_salvesatmine_failidesse():
+    pass
+
+def tesimine():
+    pass
+
+def email_saatmine():
+    pass
+
+def küsimuste_lisamine():
+    pass
